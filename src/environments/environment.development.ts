ce42e80cf2ee7cdd6a5c8568bcccfd8486504732
sr <sachinrana99@yahoo.com>
2023-06-28 18:59:01 +0530

    changes

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    subscriberId: "<REPLACE_WITH_USER_UNIQUE_IDENTIFIER>",
+    applicationIdentifier: "<REPLACE_WITH_APP_ID_FROM_ADMIN_PANEL>"
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    subscriberId: "<REPLACE_WITH_USER_UNIQUE_IDENTIFIER>",
+    applicationIdentifier: "<REPLACE_WITH_APP_ID_FROM_ADMIN_PANEL>"
+};
